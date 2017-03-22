d9c4c08951ff175b1877863861c622a163b32b39
469700143@qq.com <qzw159133>
2017-03-22 22:13:55 +0800

    新增

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :users
+   get 'static_pages/home'
+
+   get 'static_pages/help'
+  root 'users#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
